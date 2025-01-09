fc25adce60e99848d26647928112f698d1df7a9f
Alex <acarpiomoratalla@cifpfbmoll.eu>
2025-01-09 16:08:48 +0100

    Initial commit

---
+++ app/src/main/java/com/acarpio/acarpio_splash/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.acarpio.acarpio_splash;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView tv = findViewById(R.id.textView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.custom_animation);
+        tv.setOnClickListener(v -> tv.startAnimation(animation));
+    }
+}
